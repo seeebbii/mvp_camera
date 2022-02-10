76fe07b3dae584131d0c6c5f788b9cf6037a6b16
seeebbii <haseebzafar98@gmail.com>
2022-02-10 19:08:19 +0500

    CHANGES

--- lib/model/file_data_model.dart
+++ lib/model/file_data_model.dart
@@ -17,10 +17,12 @@ class FileDataModel {
 
 
 class FileDataModelForIos {
+
   File imageFile;
   edt.FlutterExif fileData;
   Map<dynamic, dynamic> metaData;
   LatLng position;
+
   FileDataModelForIos({ required this.imageFile, required this.fileData, required this.metaData, required this.position, });
 // Map<String, dynamic> toJson() => {'id':id, 'name':name };
 }
