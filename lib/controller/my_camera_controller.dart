151121ff7e2b8c30a438d42b46e962585024076f
seeebbii <haseebzafar98@gmail.com>
2022-02-11 15:25:12 +0500

    Landscape issue fix

--- lib/controller/my_camera_controller.dart
+++ lib/controller/my_camera_controller.dart
@@ -95,6 +95,7 @@ class MyCameraController extends GetxController {
         isRearCameraSelected.value = true;
         controller.value.initialize().then((value) {
           debugPrint("CAMERA INIT SUCCESS");
+          controller.value.lockCaptureOrientation(DeviceOrientation.portraitUp);
           controller.value.setFlashMode(FlashMode.off);
           initializeZoom();
         });
